0265f1cedd4a7f6726b951ea2efcca3e6e40bb3c
Abhi27122 <abhishekkote29@gmail.com>
2024-02-29 00:27:26 +0530

    5th commit

--- Abc.java
+++ Abc.java
@@ -1,7 +1,7 @@
 public class Abc{
 	public static void main(String args[]){
 		System.out.println("This code is compiled by jenkins via github");
-		System.out.println("This code is compiled by jenkins via github");
+		System.out.println("successful");
 		
 	}
 }
