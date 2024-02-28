b68c00e56c37a315e4d78be957436eb53553644e
Abhi27122 <abhishekkote29@gmail.com>
2024-02-28 23:02:25 +0530

    first commit

--- Abc.java
+++ Abc.java
@@ -1,6 +1,7 @@
 public class Abc{
 	public static void main(String args[]){
-		System.out.println("This code is compiled by jenkins");
+		System.out.println("This code is compiled by jenkins via github");
 		System.out.println("This code is runned by jenkins");
+		System.out.println("When we push the code");
 	}
 }
