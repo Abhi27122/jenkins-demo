8d848d5b988f4204e01b9f9416acc9636a5088c1
Abhi27122 <abhishekkote29@gmail.com>
2024-02-28 22:51:04 +0530

    first commit

---
+++ Abc.java
@@ -0,0 +1,6 @@
+public class Abc{
+	public static void main(String args[]){
+		System.out.println("This code is compiled by jenkins");
+		System.out.println("This code is runned by jenkins");
+	}
+}
